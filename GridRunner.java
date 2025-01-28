4f09e7467114c0e7881dc140a60ab90b2462fc06
yycholla <hanway.colin@gmail.com>
2025-01-28 15:39:34 -0700

    Finish refactor, add methods to set grids and simplified get methods. Added methods for more information and changed toString() in order to better satisfy needs. Added javaDoc documentation to each method.

--- GridRunner.java
+++ GridRunner.java
@@ -1,23 +1,25 @@
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class GridRunner {
     public static void main(String[] args) throws FileNotFoundException {
         GridMonitor test = new GridMonitor("sample.txt");
         double[][] gridData, surroundingSumGrid, surroundingAvgGrid, deltaGrid;
         boolean[][] dangerGrid;
+        ArrayList<double[][]> arrays = new ArrayList<>();
 
         gridData = test.getBaseGrid();
         surroundingSumGrid = test.getSurroundingSumGrid();
         surroundingAvgGrid = test.getSurroundingAvgGrid();
         deltaGrid = test.getDeltaGrid();
         dangerGrid = test.getDangerGrid();
+        arrays.add(gridData);
+        arrays.add(surroundingSumGrid);
+        arrays.add(surroundingAvgGrid);
+        arrays.add(deltaGrid);
 
-        for (int i = 0; i < gridData.length; i++) {
-            for (int j = 0; j < gridData[i].length; j++) {
-                System.out.println("Grid: " + gridData[i][j] + " Range: " + (surroundingAvgGrid[i][j] - deltaGrid[i][j])+ "-" + (surroundingAvgGrid[i][j] + deltaGrid[i][j])+ " danger: " + dangerGrid[i][j]);
-            }
-        }
+        System.out.println(test.toString());
 
     }
 }
