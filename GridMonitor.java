2ad158aedbf16d81c0a49229022a0abed8efa199
yycholla <hanway.colin@gmail.com>
2025-01-28 13:34:09 -0700

    init branch refactor

--- GridMonitor.java
+++ GridMonitor.java
@@ -28,7 +28,11 @@ public class GridMonitor implements GridMonitorInterface {
         }
     }
 
-    @Override
+    /**
+     * Returns a copy of the original base grid, as read from file.
+     *
+     * @return base grid
+     */
     public double[][] getBaseGrid() {
         double[][] returnGrid = new double[rows][columns];
         for (int i = 0; i < rows; i++) {
@@ -39,7 +43,15 @@ public class GridMonitor implements GridMonitorInterface {
         return returnGrid;
     }
 
-    @Override
+    /**
+     * Returns a grid with the same dimensions as the base grid, but each element
+     * is the sum of the 4 surrounding base elements. For elements on a grid border,
+     * the base element's own value is used when looking out of bounds.
+     * This method is exposed for testing.
+     *
+     * @return grid containing the sum of adjacent positions for each position in
+     *         the grid.
+     */
     public double[][] getSurroundingSumGrid() {
         double up, down, left, right, sum;
 
@@ -49,14 +61,21 @@ public class GridMonitor implements GridMonitorInterface {
                 down = (j + 1 < columns && j + 1 >= 0) ? gridData[i][j + 1] : gridData[i][j];
                 left = (i - 1 < rows && i - 1 >= 0) ? gridData[i - 1][j] : gridData[i][j];
                 right = (i + 1 < rows && i + 1 >= 0) ? gridData[i + 1][j] : gridData[i][j];
-                sum = up + down + left+ right;
+                sum = up + down + left + right;
                 surroundingSumGrid[i][j] = sum;
             }
         }
         return surroundingSumGrid;
     }
 
-    @Override
+    /**
+     * Returns the average value of the surrounding 4 base elements
+     * per grid location. Due to the cardinal directions being the elements summed,
+     * the average is always the sum devided by 4.
+     *
+     * @return grid consisting of the sum of each position devided by 4 to provide
+     *         the average.
+     */
     public double[][] getSurroundingAvgGrid() {
         surroundingSumGrid = getSurroundingSumGrid();
         for (int i = 0; i < rows; i++) {
@@ -67,7 +86,15 @@ public class GridMonitor implements GridMonitorInterface {
         return surroundingAvgGrid;
     }
 
-    @Override
+    /**
+     * In order to operate withn our safety ratings, the maximum deviation the
+     * panels can have from
+     * the average of adjacent panels is 50% of the average to either side. This
+     * method returns
+     * the delta for each panel for future calculation.
+     *
+     * @return grid consisting of the maximum deviation allowed on each panel.
+     */
     public double[][] getDeltaGrid() {
         surroundingAvgGrid = getSurroundingAvgGrid();
         for (int i = 0; i < rows; i++) {
@@ -78,13 +105,24 @@ public class GridMonitor implements GridMonitorInterface {
         return deltaGrid;
     }
 
-    @Override
+    /**
+     * In order to notify the crew of any panels operating out of spec, this method
+     * calculates
+     * the safe operating range of each panel compared to adjacent panels and
+     * compares it to it's current value.
+     * If the panel is within safe operating range the panel reports false for use
+     * in instrumentation and automation
+     *
+     * @return grid consisting of boolean operating states of the each panel. False
+     *         for no issue, true if the panel is operating in a dangerous state.
+     */
     public boolean[][] getDangerGrid() {
         deltaGrid = getDeltaGrid();
         surroundingAvgGrid = getSurroundingAvgGrid();
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                dangerGrid[i][j] = !((gridData[i][j] < (surroundingAvgGrid[i][j] + deltaGrid[i][j])) && (gridData[i][j] > (surroundingAvgGrid[i][j] - deltaGrid[i][j])));
+                dangerGrid[i][j] = !((gridData[i][j] < (surroundingAvgGrid[i][j] + deltaGrid[i][j]))
+                        && (gridData[i][j] > (surroundingAvgGrid[i][j] - deltaGrid[i][j])));
             }
         }
         return dangerGrid;
