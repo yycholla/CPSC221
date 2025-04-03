73cc44fccf9ada258c948b8752c58998dd825584
chanway <hanway.colin@gmail.com>
2025-04-03 11:14:35 -0600

    excessive desire to push

---
+++ Week10/Queues/CircularArrayQueue.java
@@ -0,0 +1,94 @@
+import java.util.Arrays;
+
+public class CircularArrayQueue<E> implements QueueADT<E> {
+
+    private final static int DEFAULT_CAPACITY = 100;
+    private int front, rear, count;
+    private E[] queue;
+
+    public CircularArrayQueue() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    @SuppressWarnings("unchecked")
+    public CircularArrayQueue(int initialCapacity) {
+        front = rear = count = 0;
+        queue = (E[])(new Object[initialCapacity]);
+    }
+
+    @Override
+    public void enqueue(E element) {
+        if (size() == queue.length) {
+            expandCapacity();
+        }
+
+        queue[rear] = element; // Insert element on rear
+        rear = (rear + 1) % queue.length; // Increment rear keeping loop
+        count++;
+    }
+
+    @Override
+    public E dequeue() {
+        if (this.isEmpty()) {
+            throw new EmptyCollectionException("Queue");
+        }
+
+        E result = queue[front]; // Grab front
+        queue[front] = null; // Null to prevent memory leaks
+        front = (front + 1) % queue.length; // increment front accounting for circular structure
+        count--; // decrement count
+        return result;
+    }
+
+    @Override
+    public E first() {
+        if (this.isEmpty()) {
+            throw new EmptyCollectionException("Queue");
+        }
+
+        return queue[front];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public int size() {
+        return count;
+    }
+
+    public String toString() {
+        String result = "[";
+        int index = this.front;
+
+        for (int i = 0; i< this.size(); i++) {
+            if (i > 0) {
+                result += ", ";
+            }
+            result += queue[index];
+            index = (index + 1) % queue.length;
+        }
+
+        result += "]";
+        return result;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void expandCapacity() {
+        // this.queue = Arrays.copyOf(queue, queue.length*2); - This does not work since it will add elements in the middle of the circle we currently have
+        E[] larger = (E[])(new Object[queue.length*2]);
+        int index = this.front;
+        for (int i = 0; i< this.size(); i++) {
+            larger[i] = queue[index];
+            index = (index + 1) % queue.length;
+        }
+
+        front = 0;
+        rear = this.size();
+        queue = larger;
+
+    }
+
+}
--- Week10/Queues/TicketCounter.java
+++ Week10/Queues/TicketCounter.java
@@ -1,13 +1,14 @@
-import java.util.LinkedList;
-import java.util.Queue;
+// import java.util.LinkedList;
+// import java.util.Queue;
 
 public class TicketCounter {
     private final static int PROCESS_TIME = 120;
     private final static int ARRIVAL_INTERVAL = 15;
-    private final static int NUM_CUSTOMERS = 100;
+    private final static int NUM_CUSTOMERS = 200;
     private final static int MAX_CASHIERS = 10;
     public static void main(String[] args) {
-        Queue<Customer> customerQueue = new LinkedList<Customer>(); // Line of customers
+        QueueADT<Customer> customerQueue = new CircularArrayQueue<Customer>(); // Line of customers
+        // Queue<Customer> customerQueue = new LinkedList<Customer>(); // Line of customers
         Customer customer; // current customer being served
         int totalTime; // Sum of all the time that all customers spent before entering the theater
         int averageTime; // Average wait time of all of the customers
@@ -18,7 +19,7 @@ public class TicketCounter {
         for (int cashiers = 1; cashiers <= MAX_CASHIERS; cashiers++) {
             // Load the customer queue with each arriving ARRIVAL_INTERVAL after the prior.
             for (int count = 1; count <= NUM_CUSTOMERS; count++) {
-                customerQueue.add(new Customer(count * ARRIVAL_INTERVAL));
+                customerQueue.enqueue(new Customer(count * ARRIVAL_INTERVAL));
             }
             // Init total customer wait time
             totalTime = 0;
@@ -31,7 +32,7 @@ public class TicketCounter {
             // Single cashier processing all customers
             while (!customerQueue.isEmpty()) {
                 for (int i = 0; i < cashiers && !customerQueue.isEmpty(); i++) {
-                    customer = customerQueue.remove();
+                    customer = customerQueue.dequeue();
 
                     // When does the cashier start to work with the customer?
                     // If there is no line
